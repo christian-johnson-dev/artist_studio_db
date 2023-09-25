151c6593e3e4af35fdf0faa569aa1d5e577b4fb7
Christian Johnson <charcoaldust@mac.com>
2023-09-25 10:19:58 -0700

    feat: seed series table

---
+++ seed_data/01_series.sql
@@ -0,0 +1,12 @@
+INSERT INTO series (
+  name, description, year_start, year_end
+)
+VALUES
+('Laser and Linoleum', 'A series of linocuts created by carving digital drawings into linoleum.', 2016, 2018),
+('20 Views of M', 'Twenty drawings from 5 poses of the model MH.', 2012, 2013),
+('Five Little Black Movements', 'A commission by Chanel, INC.', 2014, 2014),
+('Seamstress'' Tale', 'Four linocuts.', 2012, 2012),
+('Vespers', '7 Charcoal drawings inspired by Rachmaninov''s Vespers.', 1997, 1997),
+('Twelve Easy Pieces', '12 stenocut figures.', 2012, 2012),
+('Constructions', 'Six charcoal drawings on curled paper.', 2014, 2014),
+('Family Portraits', 'Portrait busts of nieces and nephews.', 2021, 2022);
