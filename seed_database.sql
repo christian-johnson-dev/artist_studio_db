f8fdfc5a15c424b64807178f75b8a0ea2a722d8d
Christian Johnson <charcoaldust@mac.com>
2023-09-25 10:19:19 -0700

    feat: import seed database queries

---
+++ seed_database.sql
@@ -0,0 +1,7 @@
+\i seed_data/01_series.sql
+\i seed_data/02_organizations.sql
+\i seed_data/03_people.sql
+\i seed_data/04_people_organizations.sql
+\i seed_data/05_artworks_originals.sql
+\i seed_data/06_supports_artwork.sql
+\i seed_data/07_artworks_editions.sql
