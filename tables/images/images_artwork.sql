ece3bb3f41aa6ae4d6ded57a0f1060b699943080
Christian Johnson <charcoaldust@mac.com>
2023-09-25 11:33:45 -0700

    feat(sql): add images_artwork table to link images and artworks
    - Introduce the `images_artwork` table for joining `images` and `artworks` tables
    - Enable multiple images to be attached to a single artwork
    - Ensure that only one image can be the main image for an artwork
    - Add a column for captions
    - Include constraints to enforce uniqueness of the main image for each artwork

---
+++ tables/images/images_artwork.sql
@@ -0,0 +1,19 @@
+--* This table joins images to artworks tables
+--* Multiple images can be attached to an artwork
+--* Only one image can be the main image for an artwork
+
+CREATE TABLE images_artwork (
+  -- * METADATA
+  image_id INT REFERENCES images (id),
+  artwork_id INT REFERENCES artworks (id),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+  is_main_image BOOLEAN NOT NULL DEFAULT FALSE,
+  caption TEXT,
+  PRIMARY KEY (image_id, artwork_id),
+  UNIQUE (artwork_id) WHERE (is_main_image = TRUE)
+);
+
+-- Path: tables/images/images_artwork.sql
+
+
