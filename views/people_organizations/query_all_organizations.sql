f37d85a9b954ad366c1258452222842ed4983588
Christian Johnson <charcoaldust@mac.com>
2023-09-26 09:49:00 -0700

    feat(view): create or replace all_organizations view
    - Create or replace view combining org_locations, organizations, addresses, phone_numbers, and emails
    - The view includes columns for ID, name, address, city, province/state, zip code, country, phone, and email
    - Use COALESCE to prioritize org_locations name over organizations name where applicable

---
+++ views/people_organizations/query_all_organizations.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW all_organizations AS
+SELECT
+	ol.id,
+	COALESCE(ol.name,og.name) AS name,
+	CONCAT_WS(', ', line_1, line_2),
+	city,
+	prov_state,
+	postal_zip AS zip,
+	country,
+	phone_number  AS phone,
+	email
+FROM
+	org_locations ol
+	JOIN organizations og ON ol.organization_id = og.id
+	JOIN addresses ad ON ad.org_location_id = ol.id
+	JOIN phone_numbers pn ON pn.org_location_id = ol.id
+	JOIN emails em ON em.org_location_id = ol.id;
