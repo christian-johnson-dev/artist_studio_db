db8b45250e244e0543269b4f51c03f942a1e2864
Christian Johnson <charcoaldust@mac.com>
2023-09-28 11:32:37 -0700

    feat(sql): refactor people_organizations table to reference org_locations
    - Replace 'organization_id' with 'org_location_id' and update foreign key reference to 'org_locations'
    - Replace 'organization_id' with 'org_location_id' in PRIMARY KEY composite

--- tables/people_organizations/people_organizations.sql
+++ tables/people_organizations/people_organizations.sql
@@ -3,15 +3,16 @@
 CREATE TABLE people_organizations (
   -- * METADATA
   people_id INT REFERENCES people (id) NOT NULL,
-  organization_id INT REFERENCES organizations (id) NOT NULL,
+  org_location_id INT REFERENCES org_locations (id) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-
   -- * ROLE
   role VARCHAR (100) NOT NULL,
 
-  PRIMARY KEY (people_id, organization_id)
+  PRIMARY KEY (people_id, org_location_id)
 );
+
+
 
 
 -- Path: tables/primary/people_locations/people_organizations.sql
