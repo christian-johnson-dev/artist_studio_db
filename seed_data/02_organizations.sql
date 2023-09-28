63d9bfef5e68cd5a8f61c8b0cf28742a80734f0f
Christian Johnson <charcoaldust@mac.com>
2023-09-28 11:41:15 -0700

    feat(seed-data): extend organizations and improve address information
    - Extend organization seed data with new entries
    - Add new organization locations with extended attributes
    - Update address information to include more details

--- seed_data/02_organizations.sql
+++ seed_data/02_organizations.sql
@@ -1,127 +1,165 @@
-
-
--- 1. Insert 17 seed records into the organizations table
-INSERT INTO organizations (
-  name, description
-)
+-- 1. Insert seed records into the organizations table
+INSERT INTO organizations ( name, description )
 VALUES
-('City Central Museum', 'A general museum with a wide range of art from classical to contemporary.'),
-('Uptown Art Gallery', 'Focused on contemporary art from emerging artists.'),
-('Greenwood Fine Arts', 'A gallery that specializes in landscape and nature-themed artworks.'),
-('Historical Arts Museum', 'Specializes in art from different historical periods and civilizations.'),
-('Downtown Art House', 'An eclectic gallery featuring both traditional and modern art.'),
-('Riverfront Print Studio', 'Offers a variety of printmaking services including linocuts and screen prints.'),
-('Metro Modern Gallery', 'A gallery dedicated to modern and abstract art.'),
-('National Heritage Museum', 'A museum that focuses on art that represents national heritage and history.'),
+('Josephine Bijoux', 'The artist whose works this database manages '), 
+('City Central Museum', 'A general museum with a wide range of art from classical to contemporary.'),  
+('Uptown Art Gallery', 'Focused on contemporary art from emerging artists.'),  
+('Greenwood Fine Arts', 'A gallery that specializes in landscape and nature-themed artworks.'),   
+('Historical Arts Museum', 'Specializes in art from different historical periods and civilizations.'),  
+('Downtown Art House', 'An eclectic gallery featuring both traditional and modern art.'),  
+('Riverfront Print Studio', 'Offers a variety of printmaking services including linocuts and screen prints.'),  
+('Metro Modern Gallery', 'A gallery dedicated to modern and abstract art.'),  
+('National Heritage Museum', 'A museum that focuses on art that represents national heritage and history.'),  
 ('Elite Fine Arts', 'A high-end gallery featuring works from internationally renowned artists.'),
-('Cultural Arts Museum', 'Dedicated to art from various cultures around the world.'),
-('Suburban Art Gallery', 'A community-focused gallery that supports local artists.'),
-('Classic Arts Museum', 'Known for its collection of classical art from the Renaissance and Baroque periods.'),
-('Local Palette Gallery', 'Promotes local artists and features a range of styles and mediums.'),
+('Cultural Arts Museum', 'Dedicated to art from various cultures around the world.'),  
+('Suburban Art Gallery', 'A community-focused gallery that supports local artists.'),  
+('Classic Arts Museum', 'Known for its collection of classical art from the Renaissance and Baroque periods.'),  
+('Local Palette Gallery', 'Promotes local artists and features a range of styles and mediums.'), 
 ('Contemporary Arts Museum', 'Focuses on contemporary art from the 21st century.'),
 ('Experimental Art Space', 'A gallery dedicated to experimental and avant-garde art.'),
 ('Artworks Foundry', 'A bronze foundry that specializes in casting sculptures.'),
-('Blumenthal-Adler Gallery', 'A gallery with multiple locations featuring the most sought after artists.');
+('Blumenthal-Adler Gallery', 'A gallery with multiple locations featuring the most sought after artists.'),
+('American Broadcasting Company', 'An American commercial broadcast radio and television network owned by Disney Media Networks division of The Walt Disney Company.'),
+('Pace Editions', 'Publisher of fine art prints'),
+('Monnacelli Press', 'Publisher of fine art books'),
+('Art Squared', 'Monthly arts magazine '),  
+('Albion Journal', 'Quarterly arts journal'), 
+('Cider Press', 'Portland cider house and literary venue'),
+('Mindful Radio', 'a podcast publisher'),
+('Pellman Auctions', 'Multi-city auction house'); 
 
-
--- 2. Insert 19 seed records into the org_locations table
-INSERT INTO org_locations (
-  organization_id, name
-)
+-- 2. Insert 30 seed records into the org_locations table
+INSERT INTO org_locations ( organization_id, name )
 VALUES
-(1, NULL),
+(1, 'Studio'),
 (2, NULL),
 (3, NULL),
 (4, NULL),
-(5, NULL),
-(6, NULL),
-(7, NULL),
-(8, NULL),
-(9, NULL),
-(10, NULL),
-(11, NULL),
-(12, NULL),
-(13, NULL),
-(14, NULL),
-(15, NULL),
-(16, NULL),
-(17, 'Blumenthal-Alder: Portland'),
-(17, 'Blumenthal-Alder: Vancouver'),
-(17, 'Blumenthal-Alder: New York');
+(5, NULL), 
+(6, NULL), 
+(7, NULL), 
+(8, NULL), 
+(9, NULL), 
+(10, NULL), 
+(11, NULL), 
+(12, NULL), 
+(13, NULL), 
+(14, NULL), 
+(15, NULL), 
+(16, NULL), 
+(17, NULL), 
+(18, 'Portland'), 
+(18, 'Vancouver'), 
+(18, 'New York'),  
+(1, 'Kroger Storage'), 
+(19, NULL),
+(20, NULL), 
+(21, NULL), 
+(22, NULL), 
+(23, NULL), 
+(24, NULL), 
+(25, NULL), 
+(26, 'New York'), 
+(26, 'Los Angeles'); 
 
-
--- 3. Insert 19 seed records into the addresses table
-INSERT INTO addresses (
-  org_location_id, address_type, line_1, line_2, city, prov_state, postal_zip, country, notes
-)
+-- 3. Insert seed records into the addresses table
+INSERT INTO addresses ( org_location_id, address_type, line_1, line_2, city, prov_state, postal_zip, country, notes )
 VALUES
-(1, 'main', '123 Museum St.', NULL, 'New York', 'NY', '10001', 'USA', NULL),
-(2, 'main', '789 Gallery Ave.', 'Suite 4', 'Chicago', 'IL', '60601', 'USA', NULL),
-(3, 'main', '456 Art Ln.', NULL, 'San Francisco', 'CA', '94101', 'USA', NULL),
-(4, 'main', '234 History Blvd.', NULL, 'Boston', 'MA', '02108', 'USA', NULL),
-(5, 'main', '1010 Main St.', NULL, 'Austin', 'TX', '73301', 'USA', NULL),
-(6, 'main', '2525 River Rd.', NULL, 'Portland', 'OR', '97201', 'USA', NULL),
-(7, 'main', '1616 Metro St.', NULL, 'Miami', 'FL', '33101', 'USA', NULL),
-(8, 'main', '200 Heritage Way', NULL, 'Washington', 'DC', '20001', 'USA', NULL),
-(9, 'main', '350 Elite Rd.', 'Floor 3', 'Los Angeles', 'CA', '90001', 'USA', NULL),
-(10, 'main', '707 Culture Dr.', NULL, 'Denver', 'CO', '80014', 'USA', NULL),
-(11, 'main', '333 Suburb Ln.', NULL, 'Minneapolis', 'MN', '55401', 'USA', NULL),
-(12, 'main', '1212 Classic Ct.', NULL, 'Philadelphia', 'PA', '19019', 'USA', NULL),
-(13, 'main', '1414 Palette Pl.', NULL, 'Atlanta', 'GA', '30301', 'USA', NULL),
-(14, 'main', '616 Modern Way', NULL, 'Seattle', 'WA', '98101', 'USA', NULL),
-(15, 'main', '505 Experiment St.', NULL, 'San Diego', 'CA', '92101', 'USA', NULL),
-(16, 'main', '404 Foundry Ave.', NULL, 'San Francisco', 'CA', '94101', 'USA', NULL),
-(17, 'main', '212 Glisan St.', NULL, 'Portland', 'OR', '97201', 'USA', NULL),
-(18, 'main', '401 Market Ave.', NULL, 'Vancouver', 'WA', '98661', 'USA', NULL),
-(19, 'main', '919 Adler Ave.', NULL, 'New York', 'NY', '10013', 'USA', NULL);
+(1, 'studio', '123 Artist St.', NULL, 'Portland', 'OR', '95203', 'USA', NULL),
+(2, 'main', '123 Museum St.', NULL, 'New York', 'NY', '10001', 'USA', NULL),
+(3, 'main', '789 Gallery Ave.', 'Suite 4', 'Chicago', 'IL', '60601', 'USA', NULL),
+(4, 'main', '456 Art Ln.', NULL, 'San Francisco', 'CA', '94101', 'USA', NULL),
+(5, 'main', '234 History Blvd.', NULL, 'Boston', 'MA', '02108', 'USA', NULL),
+(6, 'main', '1010 Main St.', NULL, 'Austin', 'TX', '73301', 'USA', NULL),
+(7, 'main', '2525 River Rd.', NULL, 'Portland', 'OR', '97201', 'USA', NULL),
+(8, 'main', '1616 Metro St.', NULL, 'Miami', 'FL', '33101', 'USA', NULL),
+(9, 'main', '200 Heritage Way', NULL, 'Washington', 'DC', '20001', 'USA', NULL),
+(10, 'main', '350 Elite Rd.', 'Floor 3', 'Los Angeles', 'CA', '90001', 'USA', NULL),
+(11, 'main', '707 Culture Dr.', NULL, 'Denver', 'CO', '80014', 'USA', NULL),
+(12, 'main', '333 Suburb Ln.', NULL, 'Minneapolis', 'MN', '55401', 'USA', NULL),
+(13, 'main', '1212 Classic Ct.', NULL, 'Philadelphia', 'PA', '19019', 'USA', NULL),
+(14, 'main', '1414 Palette Pl.', NULL, 'Atlanta', 'GA', '30301', 'USA', NULL),
+(15, 'main', '616 Modern Way', NULL, 'Seattle', 'WA', '98101', 'USA', NULL),
+(16, 'main', '505 Experiment St.', NULL, 'San Diego', 'CA', '92101', 'USA', NULL),
+(17, 'main', '404 Foundry Ave.', NULL, 'San Francisco', 'CA', '94101', 'USA', NULL),
+(18, 'main', '212 Glisan St.', NULL, 'Portland', 'OR', '97201', 'USA', NULL),
+(19, 'main', '401 Market Ave.', NULL, 'Vancouver', 'WA', '98661', 'USA', NULL),
+(20, 'main', '919 Adler Ave.', NULL, 'New York', 'NY', '10013', 'USA', NULL),
+(21, 'storage', '123 Pittock St.', NULL, 'Sunnyvale', 'NY', '10023', 'USA', NULL),
+(22, 'main', '77 W 66th St.', NULL, 'New York', 'NY', '10023', 'USA', NULL),
+(23, 'main', '521 W 57th St.', NULL, 'New York', 'NY', '10019', 'USA', NULL),
+(24, 'main', '121 W 21st St.', NULL, 'New York', 'NY', '10011', 'USA', NULL),
+(25, 'main', '123 W 18th St.', 'Unit 22', 'New York', 'NY', '10011', 'USA', NULL),
+(26, 'main', '11 E 14th St.', '#3004', 'New York', 'NY', '10010', 'USA', NULL),
+(27, 'main', '1212 Glisan St.', NULL, 'Portland', 'OR', '97201', 'USA', NULL),
+(28, 'main', '521 W 57th St.', 'No. 2022', 'New York', 'NY', '10011', 'USA', NULL),
+(29, 'main', '15 Madison Ave.', '2nd Floor', 'New York', 'NY', '10011', 'USA', NULL),
+(30, 'main', '34 Molline Way', NULL, 'Los Angeles', 'CA', '90021', 'USA', NULL);
 
-
---4. Insert 19 seed records into the phone_numbers table
-INSERT INTO phone_numbers (
-  org_location_id, phone_number, phone_type
-)
+--4. Insert seed records into the phone_numbers table
+INSERT INTO phone_numbers (org_location_id, phone_number, phone_type )
 VALUES
-(1, '212-555-0101', 'main'),
-(2, '312-555-0202', 'main'),
-(3, '415-555-0303', 'main'),
-(4, '617-555-0404', 'main'),
-(5, '512-555-0505', 'main'),
-(6, '503-555-0606', 'main'),
-(7, '305-555-0707', 'main'),
-(8, '202-555-0808', 'main'),
-(9, '323-555-0909', 'main'),
-(10, '720-555-1000', 'main'),
-(11, '612-555-1100', 'main'),
-(12, '215-555-1200', 'main'),
-(13, '404-555-1300', 'main'),
-(14, '206-555-1400', 'main'),
-(15, '619-555-1500', 'main'),
-(16, '415-555-1600', 'main'),
-(17, '503-555-1700', 'main'),
-(18, '360-555-1701', 'main'),
-(19, '212-555-1702', 'main');
+(1, '212-555-0000', 'studio'),
+(2, '212-555-0101', 'main'),
+(3, '312-555-0202', 'main'),
+(4, '415-555-0303', 'main'),
+(5, '617-555-0404', 'main'),
+(6, '512-555-0505', 'main'),
+(7, '503-555-0606', 'main'),
+(8, '305-555-0707', 'main'),
+(9, '202-555-0808', 'main'),
+(10, '323-555-0909', 'main'),
+(11, '720-555-1000', 'main'),
+(12, '612-555-1100', 'main'),
+(13, '215-555-1200', 'main'),
+(14, '404-555-1300', 'main'),
+(15, '206-555-1400', 'main'),
+(16, '619-555-1500', 'main'),
+(17, '415-555-1600', 'main'),
+(18, '503-555-1700', 'main'),
+(19, '360-555-1701', 'main'),
+(20, '212-555-1702', 'main'),
+(21, '212-555-1703', 'operations'),
+(22, '212-555-1704', 'main'),
+(23, '212-555-1705', 'main'),
+(24, '212-555-1706', 'main'),
+(25, '212-555-1707', 'main'),
+(26, '212-555-1708', 'main'),
+(27, '503-555-1709', 'events desk'),
+(28, '212-555-1710', 'production office'),
+(29, '212-555-1711', 'sales'),
+(30, '213-555-1712', 'sales');
 
---5. Insert 19 seed records into the emails table
-INSERT INTO emails (
-  org_location_id, email, email_type
-)
+--5. Insert seed records into the emails table
+INSERT INTO emails (org_location_id, email, email_type)
 VALUES
-(1, 'info@citycentralmuseum.org', 'main'),
-(2, 'contact@uptownartgallery.com', 'main'),
-(3, 'inquiries@greenwoodfinearts.com', 'main'),
-(4, 'support@historicalartsmuseum.org', 'main'),
-(5, 'hello@downtownarthouse.com', 'main'),
-(6, 'services@riverfrontprintstudio.com', 'main'),
-(7, 'info@metromoderngallery.com', 'main'),
-(8, 'contactus@nationalheritagemuseum.gov', 'main'),
-(9, 'support@elitefinearts.com', 'main'),
-(10, 'info@culturalartsmuseum.org', 'main'),
-(11, 'submissions@suburbanartgallery.com', 'main'),
-(12, 'info@classicartsmuseum.org', 'main'),
-(13, 'artists@localpalettegallery.com', 'main'),
-(14, 'info@contemporaryartsmuseum.org', 'main'),
-(15, 'inquiry@experimentalartspace.com', 'main'),
-(16, 'orders@artworksfoundry.com', 'main'),
-(17, 'portland@blumenthal-adler.com', 'main'),
-(18, 'vancouver@blumenthal-adler.com', 'main'),
-(19, 'newyork@blumenthal-adler.com', 'main');
+(1, 'josephine@studio.com', 'studio'),
+(2, 'info@citycentralmuseum.org', 'main'),
+(3, 'contact@uptownartgallery.com', 'main'),
+(4, 'inquiries@greenwoodfinearts.com', 'main'),
+(5, 'support@historicalartsmuseum.org', 'main'),
+(6, 'hello@downtownarthouse.com', 'main'),
+(7, 'services@riverfrontprintstudio.com', 'main'),
+(8, 'info@metromoderngallery.com', 'main'),
+(9, 'contactus@nationalheritagemuseum.gov', 'main'),
+(10, 'support@elitefinearts.com', 'main'),
+(11, 'info@culturalartsmuseum.org', 'main'),
+(12, 'submissions@suburbanartgallery.com', 'main'),
+(13, 'info@classicartsmuseum.org', 'main'),
+(14, 'artists@localpalettegallery.com', 'main'),
+(15, 'info@contemporaryartsmuseum.org', 'main'),
+(16, 'inquiry@experimentalartspace.com', 'main'),
+(17, 'orders@artworksfoundry.com', 'main'),
+(18, 'portland@blumenthal-adler.com', 'main'),
+(19, 'vancouver@blumenthal-adler.com', 'main'),
+(20, 'newyork@blumenthal-adler.com', 'main'),
+(21, 'sunnyvale@kroger-storage.com', 'operations'),
+(22, 'broadcast.schedule@abc.com', 'broadcast schedule'),
+(23, 'info@paceeditions.com', 'main'),
+(24, 'prepress@monnacelli.com', 'pre-press'),
+(25, 'publications@artsquared.com', 'main'),
+(26, 'info@albionjournal.org', 'main'),
+(27, 'events@ciderpress.com', 'events desk'),
+(28, 'info@mindfulradio.org', 'scheduling'),
+(29, 'ny@pellman.org', 'sales'),
+(30, 'la@pellman.org', 'sales');
