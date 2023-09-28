24ca187ba95b6c678575c492b9ef8084bd04364c
Christian Johnson <charcoaldust@mac.com>
2023-09-28 11:34:34 -0700

    feat(sql): refactor exhibitions_organizations table to reference org_locations
    - Replace 'organization_id' with 'org_location_id' and update foreign key reference to 'org_locations' table
    - Update PRIMARY KEY to use 'exhibition_id' and 'org_location_id'

--- tables/exhibitions/exhibitions_organizations.sql
+++ tables/exhibitions/exhibitions_organizations.sql
@@ -3,7 +3,7 @@
 CREATE TABLE exhibition_organizations (
   -- * METADATA
   exhibition_id INT REFERENCES exhibitions (id) NOT NULL,
-  organization_id INT REFERENCES organizations (id) NOT NULL,
+  org_location_id INT REFERENCES org_locations (id) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 
@@ -12,7 +12,7 @@ CREATE TABLE exhibition_organizations (
   closing_date DATE CHECK (closing_date >= opening_date),
   is_primary BOOLEAN NOT NULL DEFAULT FALSE,
 
-  PRIMARY KEY (exhibition_id, organization_id)
+  PRIMARY KEY (exhibition_id, org_location_id)
 );
 
 
