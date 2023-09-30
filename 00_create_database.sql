55b180f32754c5d9a8f927c002dc06b3703c05d1
Christian Johnson <charcoaldust@mac.com>
2023-09-30 12:57:40 -0700

    feat (sql): wrap db creation in transaction block

--- 00_create_database.sql
+++ 00_create_database.sql
@@ -2,13 +2,12 @@ DROP DATABASE IF EXISTS art_studio;
 
 CREATE DATABASE art_studio;
 \c art_studio;
--- BEGIN;
+BEGIN;
   \i 01_create_tables.sql
   \i 02_create_functions.sql
   \i 03_seed_database.sql
   \i 04_create_views.sql
-
--- COMMIT;
+COMMIT;
 -- Create the function that will update the updated_at column
 
 
