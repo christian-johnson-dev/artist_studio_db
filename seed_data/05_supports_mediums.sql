25263ebc9b7bc332c05883f34a42a226ee17d364
Christian Johnson <charcoaldust@mac.com>
2023-10-02 19:44:20 -0700

    feat (seed): add mediums & supports. remove joins.

--- seed_data/05_supports_mediums.sql
+++ seed_data/05_supports_mediums.sql
@@ -5,6 +5,13 @@ INSERT INTO mediums (name, description) VALUES
 ('linocut', 'A printmaking technique where a design is cut into a linoleum surface and then inked and pressed onto paper.'),
 ('woodcut', 'A relief printing technique in which an image is carved into the surface of a block of wood.'),
 ('drypoint', 'An intaglio printmaking technique where an image is incised into a plate with a hard-pointed needle.'),
+('lithograph', 'A printmaking technique where a design is drawn onto a stone or metal plate with a greasy crayon and then inked and pressed onto paper.'),
+('etching', 'An intaglio printmaking technique where a metal plate is covered with a waxy ground and then scratched to expose the metal.'),
+('aquatint', 'An intaglio printmaking technique where a metal plate is covered with a porous ground and then etched to produce areas of tone.'),
+('mezzotint', 'An intaglio printmaking technique where a metal plate is roughened with a tool called a rocker so that it will print solid black.'),
+('engraving', 'An intaglio printmaking technique where a metal plate is incised with a tool called a burin.'),
+('monotype', 'A printmaking technique where a design is painted onto a smooth surface and then transferred to paper.'),
+('monoprint', 'A printmaking technique where a design is painted onto a smooth surface and then transferred to paper.'),
 ('bronze', 'A metal alloy used primarily for casting sculptures.'),
 ('plaster', 'A mixture of lime, water, and sand that hardens over time, often used for making sculptures or molds.'),
 ('watercolor', 'A painting method using pigments suspended in a water-based solution.'),
@@ -28,59 +35,11 @@ INSERT INTO supports (name, description) VALUES
 ('paper', 'A thin material made from fibers, commonly used for drawing, printing, and watercolor painting.'),
 ('canvas', 'A heavy-duty woven fabric used as a surface for painting, usually stretched over a frame.'),
 ('linen', 'A textile made from the fibers of the flax plant, used as a support for oil painting.'),
-('panel', 'A flat, rigid surface, often made from wood, used as a support for painting or other art forms.');
-
-------------------------- SUPPORT ARTWORK JOINS -------------------------
--- Associating all drawings in the 'artwork' table with the paper support
-INSERT INTO artwork_support (artwork_id, support_id)
-SELECT a.id, s.id
-FROM artwork AS a, supports AS s
-WHERE a.category = 'drawing' 
-  AND s.name = 'paper';
-
--- Associating select paintings in the 'artwork' table with the canvas support
-INSERT INTO artwork_support (artwork_id, support_id)
-SELECT a.id, s.id
-FROM artwork AS a, supports AS s
-WHERE a.id IN (3, 12, 21, 27, 41, 45) 
-  AND s.name = 'canvas';
-
--- Associating remaining paintings in the 'artwork table with the linen support
-INSERT INTO artwork_support (artwork_id, support_id)
-SELECT a.id, s.id
-FROM artwork AS a, supports AS s
-WHERE a.category = 'painting' 
-  AND s.name = 'linen'
-  AND a.id NOT IN (3, 12, 21, 27, 41, 45);
-
-
-------------------------- MEDIUM ARTWORK JOINS -------------------------
--- Associating all drawings in the 'artwork' table with the charcoal medium
-INSERT INTO artwork_medium (artwork_id, medium_id, is_primary) 
-SELECT a.id, m.id, TRUE
-FROM artwork AS a, mediums AS m
-WHERE a.category = 'drawing' 
-  AND m.name = 'charcoal';
-
--- Associating select paintings in the 'artwork' table with the acrylic medium
-INSERT INTO artwork_medium (artwork_id, medium_id, is_primary)
-SELECT a.id, m.id, TRUE
-FROM artwork AS a, mediums AS m
-WHERE a.id IN (3, 12, 21, 27, 41, 45)
-  AND m.name = 'acrylic';
-
--- Associating some of the above selected paintings in the 'artwork' table with the colored pencil medium
-INSERT INTO artwork_medium (artwork_id, medium_id)
-SELECT a.id, m.id
-FROM artwork AS a, mediums AS m
-WHERE a.id IN (3, 12, 21, 27)
-  AND m.name = 'colored pencil';
-
--- Associating remaining paintings in the 'artwork' table with the oil medium
-INSERT INTO artwork_medium (artwork_id, medium_id, is_primary)
-SELECT a.id, m.id, TRUE
-FROM artwork AS a, mediums AS m
-WHERE 
-  a.category = 'painting'
-  AND a.id NOT IN (3, 12, 21, 27, 41, 45)
-  AND m.name = 'oil';
+('panel', 'A flat, rigid surface, often made from wood, used as a support for painting or other art forms.'),
+('plaster', 'A mixture of lime, water, and sand that hardens over time, often used for making sculptures or molds.'),
+('hydrocal', 'A general-purpose gypsum cement offering higher strengths than typical plaster products'),
+('ultracal', 'A very low expansion, gypsum cement used for producing master models and molds.'),
+('bronze', 'A metal alloy used primarily for casting sculptures.'),
+('glass', 'A hard, brittle substance made by fusing sand with soda and lime, used in various forms for artwork.'),
+('wood', 'The hard fibrous material that forms the main substance of the trunk or branches of a tree or shrub.'),
+('fired clay', 'Clay that has been fired in the kiln');
