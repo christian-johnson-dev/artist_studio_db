9b2c311791c62051b48d83e1734793a334e5800a
Christian Johnson <charcoaldust@mac.com>
2023-10-04 15:46:15 -0700

    fix: add semicolon to close query

--- tables/publications/publications_organizations.sql
+++ tables/publications/publications_organizations.sql
@@ -1,9 +1,9 @@
 CREATE TABLE publications_organizations (
-  -- * METADATA
-  publication_id INT REFERENCES publications (id) NOT NULL,
-  organization_id INT REFERENCES organizations (id) NOT NULL,
-  role_id INT REFERENCES publications_organizations_roles (id) NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (publication_id, organization_id, role_id)
-)
+    -- * METADATA
+    publication_id INT REFERENCES publications (id) NOT NULL,
+    organization_id INT REFERENCES organizations (id) NOT NULL,
+    role_id INT REFERENCES publications_organizations_roles (id) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (publication_id, organization_id, role_id)
+);
