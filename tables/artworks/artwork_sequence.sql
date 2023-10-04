dc61d6bb6888cba358e556188c3d3b4a7037af35
Christian Johnson <charcoaldust@mac.com>
2023-10-04 15:33:31 -0700

    doc: add brief description

--- tables/artworks/artwork_sequence.sql
+++ tables/artworks/artwork_sequence.sql
@@ -1,3 +1,10 @@
+/*  This table is referenced when the db generates a catalog number. 
+    A catalog number comprises the year, category, and sequence number.
+    example: 2010.01.002 would be the second drawing (category 01) cataloged in 2010.
+    This table tracks the highest sequence number for each year and category.
+    Upon generation of an artwork record the db will increment the respective sequence number for that year and category
+*/
+
 CREATE TABLE artwork_sequence (
   --* METADATA
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
