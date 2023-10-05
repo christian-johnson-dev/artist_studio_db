6046e1c0958c2fd888d9325a8b14215e6def728d
Christian Johnson <charcoaldust@mac.com>
2023-10-05 12:18:49 -0700

    fix: Correct syntax, move inline comments above

--- functions/is_valid_fraction.sql
+++ functions/is_valid_fraction.sql
@@ -1,10 +1,15 @@
---* This is a helper function to validate if the decimal portion of a number is a valid x/16th fraction.
+/* --* This is a helper function to validate if the decimal portion of a number is a valid x/16th fraction.
+        * Checks to see if the number is positive.
+        * Checks to see if the decimal is in the range of 0/16 to 15/16, inclusive.
+        * Checks to see if the decimal is a multiple of 1/16.
+*/
+
 CREATE OR REPLACE FUNCTION is_valid_fraction (value NUMERIC) RETURNS BOOLEAN AS $$
 BEGIN
     RETURN 
-        value >=0  -- This checks to see if the number is positive.
-        (value - TRUNC(value)) * 16 BETWEEN 0 AND 15  -- This checks to see if the decimal is in the range of 0/16 to 15/16, inclusive.
+        value >=0  AND
+        (value - TRUNC(value)) * 16 BETWEEN 0 AND 15
         AND
-        FLOOR((value - TRUNC(value)) * 16) = (value - TRUNC(value)) * 16; -- This checks to see if the decimal is a multiple of 1/16.
+        FLOOR((value - TRUNC(value)) * 16) = (value - TRUNC(value)) * 16; 
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
