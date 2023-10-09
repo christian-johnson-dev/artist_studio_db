54240231ad282b4a90aa9eb084e26ee0cddada9c
Christian Johnson <charcoaldust@mac.com>
2023-10-09 14:25:06 -0700

    feat(sql) add pre and post table functions

--- 00_create_database.sql
+++ 00_create_database.sql
@@ -3,10 +3,11 @@ DROP DATABASE IF EXISTS art_studio;
 CREATE DATABASE art_studio;
 \c art_studio;
 BEGIN;
-  \i 01_create_tables.sql
-  \i 02_create_functions.sql
-  \i 03_seed_database.sql
-  \i 04_create_views.sql
+  \i 01_create_functions_1.sql
+  \i 02_create_tables.sql
+  \i 03_create_functions_2.sql
+  \i 04_seed_database.sql
+  \i 05_create_views.sql
 COMMIT;
 -- Create the function that will update the updated_at column
 
