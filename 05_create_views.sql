3fa46e2036ee73a91818994f6704e5b99ee76bfa
Christian Johnson <charcoaldust@mac.com>
2023-10-09 14:35:25 -0700

    chore: rename file

--- 05_create_views.sql
+++ 05_create_views.sql
@@ -1,3 +1,3 @@
 \i views/people_organizations/query_all_organizations.sql
 \i views/people_organizations/query_all_people.sql
-\i views/exhibitions/short_bib_exhibitions.sql
+\i views/exhibitions/short_exhibitions.sql
