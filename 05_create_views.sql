ecd523547bd4c3c5409c55cc7c287192be749a8a
Christian Johnson <charcoaldust@mac.com>
2023-10-27 12:15:29 -0700

    feat(sql): add artworks_current_location.sql

--- 05_create_views.sql
+++ 05_create_views.sql
@@ -1,3 +1,4 @@
 \i views/people_organizations/query_all_organizations.sql
 \i views/people_organizations/query_all_people.sql
 \i views/exhibitions/short_exhibitions.sql
+\i views/artworks/artworks_current_location.sql
