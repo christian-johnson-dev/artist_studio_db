98e0fd43b92c7b07c3614b65c43e56dadd05b120
Christian Johnson <charcoaldust@mac.com>
2023-11-07 19:40:46 -0800

    feat(sql): make image_id and artwork_id required

--- tables/images/images_artwork.sql
+++ tables/images/images_artwork.sql
@@ -4,8 +4,8 @@
 
 CREATE TABLE images_artwork (
   -- * METADATA
-  image_id INT REFERENCES images (id),
-  artwork_id INT REFERENCES artworks (id),
+  image_id INT REFERENCES images (id) NOT NULL,
+  artwork_id INT REFERENCES artworks (id) NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   is_main_image BOOLEAN NOT NULL DEFAULT FALSE,
